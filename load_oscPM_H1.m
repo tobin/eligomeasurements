8e3a2d08da956463d88a1c470ed5d2a61f391949
Tobin Fricke <tobin@pas.rochester.edu>
2011-08-14 18:01:44 -0500

    Load SURF project data

--- load_oscPM_H1.m
+++ load_oscPM_H1.m
@@ -2,5 +2,6 @@ function msmts = load_oscPM_H1(msmt_root)
 
 msmts2 = load_H1(msmt_root, 'DCnoisecouplings/opn/H1/2010-07-02', 'opnPlot');
 msmts1 = load_H1(msmt_root, 'DCnoisecouplings/opn/H1/2010-10-11', 'opnPlot');
+msmts3 = load_H1(msmt_root, 'DCnoisecouplings/opn/H1/2005-SURF',  'opnPlot');
 
-msmts = [msmts1 msmts2];
+msmts = [msmts1 msmts2 msmts3];
